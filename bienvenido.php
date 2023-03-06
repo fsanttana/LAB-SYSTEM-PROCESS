059c05b5f33efd574cc2aa9ce1b305af2a7e4460
Santana <fsanttana@users.noreply.github.com>
2023-03-06 16:04:21 -0400

    otros
    process

--- bienvenido.php
+++ bienvenido.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio & Registro</title>
+    <title>Menu de Muestras</title>
     <link rel="stylesheet" href="css/menu.css">
 </head>
 <body>
@@ -15,15 +15,19 @@
             <div class="caja__trasera">
                 <div class="caja__trasera-login">
                     <h3>Registro de Muestras</h3>
+                    <form action="Formulario-version-1.0/index.html">
                     <button id="btn_iniciar-sesion" class="btn_muestra">Registrar muestra</button>
+                    </form>
+                    <form action="">
                     <button id="btn_ensayo" class="btn_ensayo">Registrar ensayo</button>
+                    </form>
+                    <form action="seguimiento_muestra.html">
+                        <button class="btn_seguimiento">Seguimiento de muestras</button>
+                    </form>
                 </div>
 
     </main>
 
-
-
-    <script src="js/script.js"></script>
 
 </body>
 </html>
--- registro_lab.php
+++ registro_lab.php
@@ -4,31 +4,57 @@ use function PHPSTORM_META\elementType;
 
 include 'php/conexion.php';
 
-$sample = $_POST['Sample'];
+$sampleid = $_POST['Sample_ID'];
 $structure = $_POST['Structure'];
 $area = $_POST['Area'];
 $source = $_POST['Source'];
-$depthf = $_POST['Depth From'];
-$deptht = $_POST['Depth To'];
-$materialt = $_POST['Material Type'];
-$samplet = $_POST['Sample Type'];
+$depthf = $_POST['Depth_From'];
+$deptht = $_POST['Depth_To'];
+$materialt = $_POST['Material_Type'];
+$samplet = $_POST['Sample_Type'];
 $north = $_POST['North'];
 $east = $_POST['East'];
 $elev = $_POST['Elev'];
+$mcoven = $_POST['MC_Oven'];
+$mcstove = $_POST['MC_Stove'];
+$mcscale = $_POST['MC_Scale'];
+$atterberglimit = $_POST['Atterberg_Limit'];
+$grainsize = $_POST['Grain_Size'];
+$standardproctor = $_POST['Standard_Proctor'];
+$specificgravity = $_POST['Specific_Gravity'];
+$acidreactivity = $_POST['Acid_Reactivity'];
+$sandcastle = $_POST['Sand_Castle'];
+$losangelesabrasion = $_POST['Los_Angeles_Abrasion'];
+$soundness = $_POST['Soundness'];
+$ucs = $_POST['UCS'];
+$plt = $_POST['PLT'];
+$bts = $_POST['BTS'];
+$hydrometer = $_POST['Hydrometer'];
+$doublehydrometer = $_POST['Double_Hydrometer'];
+$pinhole = $_POST['Pinhole'];
+$consolidation = $_POST['Consolidation'];
+$permeability = $_POST['Permeability'];
+$sampledate = $_POST['Sample_Date'];
+$sampleby = $_POST['Sample_By'];
 $comments = $_POST['Comments'];
 
-$query = "INSERT INTO lab_test_requisition_form(Sample_ID, Structure, Area, Source, 
-Depth_From, Depth_To, Material_Type, Sample_Type, North, East, Elev, Comment) 
-VALUES('$sample', '$structure', '$area', '$source', '$depthf', '$deptht', 
-         '$materialt', '$samplet', '$north', '$east', '$elev', '$comments')";
+$sql = "INSERT INTO lab_test_requisition_form(Sample_ID, Structure, Area, Source, 
+Depth_From, Depth_To, Material_Type, Sample_Type, North, East, Elev, MC_Oven, MC_Stove, 
+MC_Scale, Atterberg_Limit, Grain_Size, Standard_Proctor, Specific_Gravity, Acid_Reactivity, 
+Sand_Castle, Los_Angeles_Abrasion, Soundness, UCS, PLT, BTS, Hydrometer, Double_Hydrometer, 
+Pinhole, Consolidation, Permeability, Comment, Sample_Date, Sample_By)
+VALUES('$sampleid', '$structure', '$area', '$source', '$depthf', '$deptht', 
+         '$materialt', '$samplet', '$north', '$east', '$elev', '$mcoven', '$mcstove', '$mcscale', '$atterberglimit', '$grainsize', '$standardproctor', '$specificgravity', 
+         '$acidreactivity', '$sandcastle', '$losangelesabrasion', '$soundness', '$ucs', '$plt', '$bts', '$hydrometer', '$doublehydrometer', '$pinhole', 
+         '$consolidation', '$permeability', '$comments', '$sampledate', '$sampleby')";
 
-$ejecutar = mysqli_query($conexion, $query);
+$ejecutar = mysqli_query($conexion, $sql);
 
 if($ejecutar){
     echo '
         <script>
-            alert("Registrado correctamente");
-            
+            alert("Muestra Registrada Correctamente.");
+            window.location = "Formulario-version-1.0/index.html";
         </script>
     ';
 }
@@ -36,8 +62,8 @@ if($ejecutar){
 else{
     echo '
         <script>
-            alert("Registro no completado");
-            
+            alert("Verifique Los Datos, El ID Digitado Esta Siendo Usado.");
+            window.location = "Formulario-version-1.0/index.html";
         </script>
     ';
 }
