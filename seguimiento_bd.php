4f20a2dae3c32c5b0685009b02520e84a0e0e30e
Santana <fsanttana@users.noreply.github.com>
2023-03-07 16:04:16 -0400

    otros
    procss
    
    Co-Authored-By: fsanttana <81333785+fsanttana@users.noreply.github.com>

--- seguimiento_bd.php
+++ seguimiento_bd.php
@@ -3,32 +3,8 @@
 
 include 'php/conexion.php';
 
-$sampleid = 'Sample_ID';
-$fechaip = 'Fecha_inicio_Preparacion';
-$fechatip = 'Fecha_Termino_Preparacion';
-$preparacionhp = 'Preparacion_Hecha_Por';
-$fechair = 'Fecha_Inicio_Realizacion';
-$fechatr = 'Fecha_Termino_Realizacion';
-$realizacionhp = 'Realizacion_Hecha_Por';
-$fechae = 'Fecha_Entrega';
-$entregahp = 'Entrega_Hecha_Por';
-$idsample = 'ID_Sample';
 
-
-$sql = mysqli_query($conexion, "SELECT * FROM seguimiento_de_muestras ")
-
-
-
-
-
-
-
-
-
-
-
-
-
+$sql = mysqli_query($conexion, "SELECT * FROM lab_test_requisition_form");
 
 
 
